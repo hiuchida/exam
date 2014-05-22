aa598c298250c57b607a7459ab0844359cdfac49
hiuchida <hiuchida@beacon-it.co.jp>
2014-05-22 16:29:26 +0900

    add weak

---
+++ java/weak/MapTest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<Object,Object> map1 = new HashMap<>();
+        test(map1);
+        Map<Object,Object> map2 = new WeakHashMap<>();
+        test(map2);
+    }
+    
+    static void test(Map<Object,Object> map) {
+        System.out.println(map.getClass().getName());
+        map.put(new String("key"), new String("value"));
+        System.out.println(map.toString());
+        System.out.println("gc");
+        System.gc();
+        System.out.println(map.toString());
+        System.out.println("");
+    }
+}
---
+++ java/weak/MapTest2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MapTest2 {
+    public static void main(String[] args) {
+        Map<Object,Object> map = new WeakHashMap<>();
+        test(map);
+    }
+    
+    static void test(Map<Object,Object> map) {
+        String[] keys = new String[4];
+        String[] vals = new String[4];
+        System.out.println(map.getClass().getName());
+        for (int i=0; i<keys.length; i++) {
+            keys[i] = new String("key" + i);
+            vals[i] = new String("val" + i);
+            map.put(keys[i], vals[i]);
+        }
+        System.out.println(map.toString());
+        System.out.println("gc");
+        System.gc();
+        System.out.println(map.toString());
+        keys[1] = null;
+        vals[2] = null;
+        keys[3] = null;
+        vals[3] = null;
+        System.out.println("gc");
+        System.gc();
+        System.out.println(map.toString());
+        System.out.println("");
+    }
+}
