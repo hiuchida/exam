fe02817a1f1e930f0b8c1e3357430373ddeeb771
hiuchida <hiuchida@beacon-it.co.jp>
2014-06-11 11:37:07 +0900

    add string_array

---
+++ java/string_array/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringArray sa = new StringArray();
+        sa.add("one");
+        sa.add("two");
+        sa.add("three");
+        System.out.println("" + sa);
+        sa.save("test.txt");
+        sa.clear();
+        sa.load("test.txt");
+        System.out.println("" + sa);
+        StringArray sa2 = new StringArray(10);
+        System.out.println("" + sa2);
+        StringArray sa3 = new StringArray(sa);
+        System.out.println("" + sa3);
+    }
+}
