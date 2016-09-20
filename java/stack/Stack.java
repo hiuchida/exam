e2859622e6a794d0382a206eb65fce272f1f57d0
hiuchida <hiro4uchida@gmail.com>
2016-09-20 21:26:59 +0900

    スタックサイズ調査追加

---
+++ java/stack/Stack.java
@@ -0,0 +1,21 @@
+package stack;
+
+public class Stack {
+	static void func(int i) {
+		int i1 = 1;
+		int i2 = 1;
+		int i3 = 1;
+		int i4 = 1;
+		int i5 = 1;
+		int i6 = 1;
+		int i7 = 1;
+		int i8 = 1;
+		int i9 = 1;
+		int i10 = 1;
+		System.out.println(i);
+		func(i+1);
+	}
+	public static void main(String[] args) {
+		func(1);
+	}
+}
