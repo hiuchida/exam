6becf9125977cdebcb567927ec13d723c24d76ed
hiuchida <hiro4uchida@gmail.com>
2016-10-17 00:08:11 +0900

    AGC005

--- java/atcoder/agc005/B.java
+++ java/atcoder/agc005/B.java
@@ -2,11 +2,20 @@ package atcoder.agc005;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class B {
+	static boolean bElapsed = false;
+	List<String> list = new ArrayList<>();
+	Set<String> set = new HashSet<>();
+	Map<String,String> map = new HashMap<>();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	void solve(BufferedReader br) throws Exception {
 		String line = br.readLine();
 		int n = Integer.parseInt(line);
 		line = br.readLine();
@@ -25,7 +34,34 @@ public class B {
 				sum += min;
 			}
 		}
-		System.out.println(sum);
+		pln(sum);
 	}
 
+	void p(char c) {
+		System.out.print(c);
+	}
+	void pln(char c) {
+		System.out.println(c);
+	}
+	void p(long l) {
+		System.out.print(l);
+	}
+	void pln(long l) {
+		System.out.println(l);
+	}
+	void p(String s) {
+		System.out.print(s);
+	}
+	void pln(String s) {
+		System.out.println(s);
+	}
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		new B().solve(br);
+		long end = System.currentTimeMillis();
+		if (bElapsed) {
+			System.out.println((end-start) + "ms");
+		}
+	}
 }
