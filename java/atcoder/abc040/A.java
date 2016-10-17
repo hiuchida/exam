fd35fb7c7c04226e96c66cf35f1f13931ff35575
hiuchida <hiro4uchida@gmail.com>
2016-10-17 12:58:21 +0900

    ABC040

---
+++ java/atcoder/abc040/A.java
@@ -0,0 +1,59 @@
+package atcoder.abc040;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class A {
+	static boolean bElapsed = false;
+	StringBuilder sb = new StringBuilder();
+	List<String> list = new ArrayList<>();
+	Set<String> set = new HashSet<>();
+	Map<String,String> map = new HashMap<>();
+
+	void solve(BufferedReader br) throws Exception {
+		String line = br.readLine();
+		String[] flds = line.split(" ");
+		int n = Integer.parseInt(flds[0]);
+		int x = Integer.parseInt(flds[1]);
+		int m1 = x-1;
+		int mx = n-x;
+		int rc = Math.min(m1, mx);
+		pln(rc);
+	}
+
+	void p(char c) {
+		System.out.print(c);
+	}
+	void pln(char c) {
+		System.out.println(c);
+	}
+	void p(long l) {
+		System.out.print(l);
+	}
+	void pln(long l) {
+		System.out.println(l);
+	}
+	void p(String s) {
+		System.out.print(s);
+	}
+	void pln(String s) {
+		System.out.println(s);
+	}
+	//Integer.MAX_VALUE=2147483647>10^9
+	//Long.MAX_VALUE=9223372036854775807L>10^18
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		new A().solve(br);
+		long end = System.currentTimeMillis();
+		if (bElapsed) {
+			System.out.println((end-start) + "ms");
+		}
+	}
+}
