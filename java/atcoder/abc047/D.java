8293c589bd2e89577a1d8dc45689100d820f4cdc
hiuchida <hiro4uchida@gmail.com>
2016-11-06 23:55:51 +0900

    abc047

---
+++ java/atcoder/abc047/D.java
@@ -0,0 +1,170 @@
+package atcoder.abc047;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class D {
+	final int _intMax = Integer.MAX_VALUE; //=2147483647>10^9
+	final long _longMax = Long.MAX_VALUE; //=9223372036854775807L>10^18
+	static boolean bElapsed = false;
+	StringBuilder sb = new StringBuilder();
+	List<Integer> list = new ArrayList<>();
+	Set<String> set = new HashSet<>();
+	Map<String,String> map = new HashMap<>();
+	Queue<String> queue = new ArrayDeque<>();
+	Deque<String> stack = new ArrayDeque<>();
+
+	void solve() {
+		int[] ia = readNums();
+		int n = ia[0];
+		int t = ia[1];
+		ia = readNums();
+		int buy = _intMax;
+		int maxdif = 0;
+		int cnt = 0;
+		for (int i=0; i<n; i++) {
+			buy = Math.min(buy, ia[i]);
+			int sel = ia[i];
+			int dif = sel - buy;
+			if (maxdif < dif) {
+				cnt = 1;
+				maxdif = dif;
+			} else if (maxdif == dif) {
+				cnt++;
+			}
+		}
+		int ans=Math.min(cnt, t/2);
+		pln(cnt);
+	}
+
+	class Point {
+		int x;
+		int y;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		public boolean equals(Object o) {
+			if (o instanceof Point) {
+				Point that = (Point)o;
+				return (x == that.x) && (y == that.y);
+			}
+			return false;
+		}
+		public int hashCode() {
+			return x + (y << 16);
+		}
+		public String toString() {
+			return "(" + x + ", " + y + ")";
+		}
+	}
+	class Info implements Comparable<Info> {
+		int idx;
+		int val;
+		public Info(int idx, int val) {
+			this.idx = idx;
+			this.val = val;
+		}
+		public int compareTo(Info o) {
+			return idx - o.idx;
+		}
+		public boolean equals(Object o) {
+			if (o instanceof Info) {
+				Info that = (Info)o;
+				return 0 == compareTo(that);
+			}
+			return false;
+		}
+		public int hashCode() {
+			return idx + (val << 16);
+		}
+		public String toString() {
+			return "(" + idx + ", " + val + ")";
+		}
+	}
+	class InfoComp implements Comparator<Info> {
+		public int compare(Info o1, Info o2) {
+			return o1.val - o2.val;
+		}
+	}
+	int pint(String s) {
+		return Integer.parseInt(s);
+	}
+	String readLine() {
+		try {
+			_line = _in.readLine();
+			return _line;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	int readNum() {
+		readLine();
+		return pint(_line);
+	}
+	String[] readFields() {
+		readLine();
+		_flds = _line.split(" ");
+		return _flds;
+	}
+	int[] readNums() {
+		readFields();
+		_nums = new int[_flds.length];
+		for (int i=0; i<_flds.length; i++) {
+			_nums[i] = pint(_flds[i]);
+		}
+		return _nums;
+	}
+	void p(char c) {
+		_out.print(c);
+	}
+	void pln(char c) {
+		_out.println(c);
+	}
+	void p(double d) {
+		_out.print(d);
+	}
+	void pln(double d) {
+		_out.println(d);
+	}
+	void p(long l) {
+		_out.print(l);
+	}
+	void pln(long l) {
+		_out.println(l);
+	}
+	void p(String s) {
+		_out.print(s);
+	}
+	void pln(String s) {
+		_out.println(s);
+	}
+	String _line;
+	String[] _flds;
+	int[] _nums;
+	static BufferedReader _in;
+	static PrintWriter _out;
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		_in = new BufferedReader(new InputStreamReader(System.in));
+		_out = new PrintWriter(System.out);
+		new D().solve();
+		_out.flush();
+		long end = System.currentTimeMillis();
+		if (bElapsed) {
+			System.err.println((end-start) + "ms");
+		}
+	}
+}
