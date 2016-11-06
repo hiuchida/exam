19fd7e66e1bedbf11adc7597b3e1c7e07086eba6
hiuchida <hiro4uchida@gmail.com>
2016-11-06 23:55:30 +0900

    atcoder

--- java/atcoder/Main.java
+++ java/atcoder/Main.java
@@ -1,78 +1,161 @@
 package atcoder;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class Main {
-	static boolean bElapsed = true;
+	final int _intMax = Integer.MAX_VALUE; //=2147483647>10^9
+	final long _longMax = Long.MAX_VALUE; //=9223372036854775807L>10^18
+	static boolean bElapsed = false;
 	StringBuilder sb = new StringBuilder();
 	List<String> list = new ArrayList<>();
 	Set<String> set = new HashSet<>();
 	Map<String,String> map = new HashMap<>();
+	Queue<String> queue = new ArrayDeque<>();
+	Deque<String> stack = new ArrayDeque<>();
 
-	void solve(BufferedReader br) throws Exception {
-		String line = br.readLine();
-		int a = Integer.parseInt(line);
+	void solve() {
+		int a = readNum();
 		BigInteger ba = BigInteger.valueOf(a);
-		line = br.readLine();
-		String[] flds = line.split(" ");
-		int b = Integer.parseInt(flds[0]);
-		int c = Integer.parseInt(flds[1]);
-		line = br.readLine();
+		int[] ia = readNums();
+		int b = ia[0];
+		int c = ia[1];
+		String line = readLine();
 		for (int i=0; i<line.length(); i++) {
 			char ch = line.charAt(i);
 		}
 		pln("" + (a+b+c) + " " + line);
 	}
 
-	class Person {
-		int no;
-		int a;
-		public Person(int no, int a) {
-			this.no = no;
-			this.a = a;
+	class Point {
+		int x;
+		int y;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		public boolean equals(Object o) {
+			if (o instanceof Point) {
+				Point that = (Point)o;
+				return (x == that.x) && (y == that.y);
+			}
+			return false;
+		}
+		public int hashCode() {
+			return x + (y << 16);
+		}
+		public String toString() {
+			return "(" + x + ", " + y + ")";
 		}
 	}
-	class PersonComp implements Comparator<Person> {
-		public int compare(Person o1, Person o2) {
-			return o2.a - o1.a;
+	class Info implements Comparable<Info> {
+		int idx;
+		int val;
+		public Info(int idx, int val) {
+			this.idx = idx;
+			this.val = val;
 		}
+		public int compareTo(Info o) {
+			return idx - o.idx;
+		}
+		public boolean equals(Object o) {
+			if (o instanceof Info) {
+				Info that = (Info)o;
+				return 0 == compareTo(that);
+			}
+			return false;
+		}
+		public int hashCode() {
+			return idx + (val << 16);
+		}
+		public String toString() {
+			return "(" + idx + ", " + val + ")";
+		}
+	}
+	class InfoComp implements Comparator<Info> {
+		public int compare(Info o1, Info o2) {
+			return o1.val - o2.val;
+		}
+	}
+	int pint(String s) {
+		return Integer.parseInt(s);
+	}
+	String readLine() {
+		try {
+			_line = _in.readLine();
+			return _line;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	int readNum() {
+		readLine();
+		return pint(_line);
+	}
+	String[] readFields() {
+		readLine();
+		_flds = _line.split(" ");
+		return _flds;
+	}
+	int[] readNums() {
+		readFields();
+		_nums = new int[_flds.length];
+		for (int i=0; i<_flds.length; i++) {
+			_nums[i] = pint(_flds[i]);
+		}
+		return _nums;
 	}
 	void p(char c) {
-		System.out.print(c);
+		_out.print(c);
 	}
 	void pln(char c) {
-		System.out.println(c);
+		_out.println(c);
+	}
+	void p(double d) {
+		_out.print(d);
+	}
+	void pln(double d) {
+		_out.println(d);
 	}
 	void p(long l) {
-		System.out.print(l);
+		_out.print(l);
 	}
 	void pln(long l) {
-		System.out.println(l);
+		_out.println(l);
 	}
 	void p(String s) {
-		System.out.print(s);
+		_out.print(s);
 	}
 	void pln(String s) {
-		System.out.println(s);
+		_out.println(s);
 	}
-	//Integer.MAX_VALUE=2147483647>10^9
-	//Long.MAX_VALUE=9223372036854775807L>10^18
-	public static void main(String[] args) throws Exception {
+	String _line;
+	String[] _flds;
+	int[] _nums;
+	static BufferedReader _in;
+	static PrintWriter _out;
+	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		new Main().solve(br);
+		_in = new BufferedReader(new InputStreamReader(System.in));
+		_out = new PrintWriter(System.out);
+		new Main().solve();
+		_out.flush();
 		long end = System.currentTimeMillis();
 		if (bElapsed) {
-			System.out.println((end-start) + "ms");
+			System.err.println((end-start) + "ms");
 		}
 	}
 }
