8a3f10c92f8793234df90d065db4e8ed3982b047
hiuchida <hiro4uchida@gmail.com>
2016-12-05 13:16:05 +0900

    codeiq

---
+++ java/codeiq/Birthday201611.java
@@ -0,0 +1,99 @@
+package codeiq;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Birthday201611 {
+	final int _intMax = Integer.MAX_VALUE; //=2147483647>10^9
+	final long _longMax = Long.MAX_VALUE; //=9223372036854775807L>10^18
+	static boolean bElapsed = false;
+	List<Integer> list = new ArrayList<>();
+
+	void solve() {
+		while (true) {
+			String line = readLine();
+			if (line == null) break;
+			int v = pint(line);
+			list.add(v);
+		}
+		Collections.sort(list);
+		int minMax = list.get(0) * list.get(1);
+		int maxMax = list.get(list.size()-2) * list.get(list.size()-1);
+		int max = Math.max(minMax, maxMax);
+		pln(max);
+	}
+
+	int pint(String s) {
+		return Integer.parseInt(s);
+	}
+	String readLine() {
+		try {
+			_line = _in.readLine();
+			return _line;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	int readNum() {
+		readLine();
+		return pint(_line);
+	}
+	String[] readFields() {
+		readLine();
+		_flds = _line.split(" ");
+		return _flds;
+	}
+	int[] readNums() {
+		readFields();
+		_nums = new int[_flds.length];
+		for (int i=0; i<_flds.length; i++) {
+			_nums[i] = pint(_flds[i]);
+		}
+		return _nums;
+	}
+	void p(char c) {
+		_out.print(c);
+	}
+	void pln(char c) {
+		_out.println(c);
+	}
+	void p(double d) {
+		_out.print(d);
+	}
+	void pln(double d) {
+		_out.println(d);
+	}
+	void p(long l) {
+		_out.print(l);
+	}
+	void pln(long l) {
+		_out.println(l);
+	}
+	void p(String s) {
+		_out.print(s);
+	}
+	void pln(String s) {
+		_out.println(s);
+	}
+	String _line;
+	String[] _flds;
+	int[] _nums;
+	static BufferedReader _in;
+	static PrintWriter _out;
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		_in = new BufferedReader(new InputStreamReader(System.in));
+		_out = new PrintWriter(System.out);
+		new Birthday201611().solve();
+		_out.flush();
+		long end = System.currentTimeMillis();
+		if (bElapsed) {
+			System.err.println((end-start) + "ms");
+		}
+	}
+}
